2a5f2bacd82eb7bb8add634995a7c2032b85b3b9
nguyhai <tenth11@gmail.com>
2018-09-16 14:43:05 -0500

    Add project files.

---
+++ RestaurantBill/Form1.Designer.vb
@@ -0,0 +1,129 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmRestaurantBill
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.txtPizza = New System.Windows.Forms.TextBox()
+        Me.txtFries = New System.Windows.Forms.TextBox()
+        Me.txtDrinks = New System.Windows.Forms.TextBox()
+        Me.btnCalc = New System.Windows.Forms.Button()
+        Me.lstDisplay = New System.Windows.Forms.ListBox()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(37, 30)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(108, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "How man pizza slices"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(69, 61)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(79, 13)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "How many fries"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(37, 94)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(108, 13)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "How many soft drinks"
+        '
+        'txtPizza
+        '
+        Me.txtPizza.Location = New System.Drawing.Point(178, 27)
+        Me.txtPizza.Name = "txtPizza"
+        Me.txtPizza.Size = New System.Drawing.Size(56, 20)
+        Me.txtPizza.TabIndex = 3
+        '
+        'txtFries
+        '
+        Me.txtFries.Location = New System.Drawing.Point(178, 58)
+        Me.txtFries.Name = "txtFries"
+        Me.txtFries.Size = New System.Drawing.Size(56, 20)
+        Me.txtFries.TabIndex = 4
+        '
+        'txtDrinks
+        '
+        Me.txtDrinks.Location = New System.Drawing.Point(178, 91)
+        Me.txtDrinks.Name = "txtDrinks"
+        Me.txtDrinks.Size = New System.Drawing.Size(56, 20)
+        Me.txtDrinks.TabIndex = 5
+        '
+        'btnCalc
+        '
+        Me.btnCalc.Location = New System.Drawing.Point(266, 24)
+        Me.btnCalc.Name = "btnCalc"
+        Me.btnCalc.Size = New System.Drawing.Size(92, 86)
+        Me.btnCalc.TabIndex = 6
+        Me.btnCalc.Text = "Calculate Total Cost"
+        Me.btnCalc.UseVisualStyleBackColor = True
+        '
+        'lstDisplay
+        '
+        Me.lstDisplay.FormattingEnabled = True
+        Me.lstDisplay.Location = New System.Drawing.Point(25, 136)
+        Me.lstDisplay.Name = "lstDisplay"
+        Me.lstDisplay.Size = New System.Drawing.Size(333, 147)
+        Me.lstDisplay.TabIndex = 7
+        '
+        'frmRestaurantBill
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(387, 297)
+        Me.Controls.Add(Me.lstDisplay)
+        Me.Controls.Add(Me.btnCalc)
+        Me.Controls.Add(Me.txtDrinks)
+        Me.Controls.Add(Me.txtFries)
+        Me.Controls.Add(Me.txtPizza)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "frmRestaurantBill"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Restaurant Bill"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents txtPizza As TextBox
+    Friend WithEvents txtFries As TextBox
+    Friend WithEvents txtDrinks As TextBox
+    Friend WithEvents btnCalc As Button
+    Friend WithEvents lstDisplay As ListBox
+End Class
---
+++ RestaurantBill/Form1.vb
@@ -0,0 +1,52 @@
+﻿Public Class frmRestaurantBill
+
+    ' Add it up here so we can change it easier. We could have also added it in calculate method. 
+    Const pizzaPrice As Double = 1.75
+    Const friesPrice As Double = 2.0
+    Const drinkPrice As Double = 1.25
+
+    Private Sub btnCalc_Click(sender As Object, e As EventArgs) Handles btnCalc.Click
+        ' Clear list box on click
+        lstDisplay.Items.Clear()
+
+        ' Delcare variables
+        Dim numOfPizza As Integer
+        Dim numOfFries As Integer
+        Dim numOfDrinks As Integer
+        Dim totalCost As Double
+
+        If Integer.TryParse(txtPizza.Text, numOfPizza) And Integer.TryParse(txtFries.Text, numOfFries) And Integer.TryParse(txtDrinks.Text, numOfDrinks) Then
+
+            ' Need 2 sub procedures, one for the ordering, and one to calculate the total cost, and one to display the bill
+            totalCost = Calculate(numOfPizza, numOfFries, numOfDrinks)
+            Display(totalCost, numOfPizza, numOfFries, numOfDrinks)
+        Else
+            MessageBox.Show("Please enter valid inputs", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End If
+    End Sub
+    Private Function Calculate(numOfPizza As Integer, numOfFries As Integer, numOfDrinks As Integer) As Double
+        Dim totalCost As Double
+        totalCost = numOfPizza * pizzaPrice + numOfFries * friesPrice + numOfDrinks * drinkPrice
+        Return totalCost
+    End Function
+    Private Sub Display(totalCost As Double, numOfPizza As Integer, numOfFries As Integer, numOfDrinks As Integer)
+        ' This will probably be long - Display only items that are ordered
+        lstDisplay.Items.Add("Item" + vbTab + vbTab + vbTab + "Quantity" + vbTab + "Price")
+        lstDisplay.Items.Add("")
+
+        If numOfPizza > 0 Then
+            lstDisplay.Items.Add("Pizza Slices" + vbTab + vbTab + numOfPizza.ToString() + vbTab + FormatCurrency(numOfPizza * pizzaPrice))
+        End If
+        If numOfFries > 0 Then
+            lstDisplay.Items.Add("Fries" + vbTab + vbTab + vbTab + numOfFries.ToString() + vbTab + FormatCurrency(numOfFries * friesPrice))
+        End If
+        If numOfDrinks > 0 Then
+            lstDisplay.Items.Add("Drinks" + vbTab + vbTab + vbTab + numOfDrinks.ToString() + vbTab + FormatCurrency(numOfDrinks * drinkPrice))
+        End If
+
+        lstDisplay.Items.Add("")
+        lstDisplay.Items.Add("Total Cost: " & totalCost.ToString("C2"))
+    End Sub
+
+
+End Class
